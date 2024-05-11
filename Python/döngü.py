10282c42a4493e27c160181e373f2ae8987c191a
Gokalp Gulkan <gokalp.gulkan@hotmail.com>
2024-05-11 16:05:21 +0300

    Create döngü.py

---
+++ Python/döngü.py
@@ -0,0 +1,22 @@
+
+
+# aralik belirterek for dongusunu calistirmak -> range
+
+for item in range(1, 101):
+    if item % 5 == 0:
+        print(item)
+
+# liste icindeki verileri tek tek islemek
+users = ["Aysen", "Fatma", "Cigdem", "Hasan", "Ahmet", "Mehmet"]
+
+
+for user in users:
+    print(user)
+
+
+print("*" * 30)
+
+# item ve index bilgisi ile listleri dongude kullanmak -> enumerate
+for obj in enumerate(users):
+    #print(obj[0], obj[1])
+    print(users[obj[0]])
