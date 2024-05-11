8157f53b91e7ae0654c424328065ed7a8e867aa0
Gokalp Gulkan <gokalp.gulkan@hotmail.com>
2024-05-11 16:05:19 +0300

    Create fonksiyon.py

---
+++ Python/fonksiyon.py
@@ -0,0 +1,21 @@
+# fonksiyonlar def ile tanimlanir
+# fonksiyonlar bir veya birden daha fazla deger/parametre alabilir veya almayabilir..
+# fonksiyonlara gonderilen parametreler default deger icerebilir..
+# fonksiyonlar bir tip veri donebilir.. (return)
+# fonksiyonlar tekrar kendilerini cagirabilirler..
+# fonksiyonlara gonderilen bilgilerin sayisi belli olmadan da kullanilabilir..
+
+def hello():
+    print("Merhaba")
+
+
+hello()
+
+name = input("adınızı yazınız:")
+
+def greetings():
+    # global degisken bilgisini kullanabilir ama cooook riskli..
+    print(f"Merhaba {name}")
+
+greetings()
+
